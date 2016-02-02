fe09724c73f2b9aeb89b2d273c2bd02f53d1230d
zdsherlock <zd5024@gmail.com>
2016-02-02 16:53:14 +0900

    v1.15
    修正图片自适应效果，相对应的修改了设计的方式

--- 1/js/nav.js
+++ 1/js/nav.js
@@ -3,22 +3,4 @@ $(document).ready(function(){
         $("#products-menu > ul").css("display", "block");},function(){
         $("#products-menu > ul").css("display", "none");   
    });//导航栏下拉菜单
-   function nav(url){
-       window.location.href = url
-   }
-   $(".nav > ul > li:first").on("click",function(){
-       nav("index.html");
-   });
-    $(".nav > ul > li:nth-child(2)").on("click",function(){
-       nav("products-index-fruits.html");
-   });
-    $(".nav > ul > li:nth-child(3)").on("click",function(){
-       nav("aboutus.html");
-   });
-    $(".nav > ul > li:nth-child(4)").on("click",function(){
-       nav("news-index.html");
-   });
-    $(".nav > ul > li:last-child").on("click",function(){
-       nav("contactus.html");
-   });
 });
